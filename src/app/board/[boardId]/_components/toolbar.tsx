6819c62dbf1cb858b945d747cd9efdc2e3d2f21b
Raavi Narayana <114774469+Narayana212@users.noreply.github.com>
2024-02-06 18:34:40 +0530

    room tool bar

--- src/app/board/[boardId]/_components/toolbar.tsx
+++ src/app/board/[boardId]/_components/toolbar.tsx
@@ -24,29 +24,52 @@ export default function Toolbar() {
           onClick={() => {}}
           isActive={false}
         />
-        <div>
-          <Square />
-        </div>
-        <div>
-          <Circle />
-        </div>
-        <div>
-          <MousePointer2 />
-        </div>
-        <div>
-          <Type />
-        </div>
-        <div>
-            <StickyNote/>
-        </div>
+        
+        <ToolButton
+          label="Text"
+          icon={Type}
+          onClick={() => {}}
+          isActive={false}
+        />
+        
+        <ToolButton
+          label="Sticky note"
+          icon={StickyNote}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Rectangle"
+          icon={Square}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Ellipse"
+          icon={Circle}
+          onClick={() => {}}
+          isActive={false}
+        />
+        <ToolButton
+          label="Pen"
+          icon={Pencil}
+          onClick={() => {}}
+          isActive={false}
+        />
       </div>
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-        <div>
-          <Undo2 />
-        </div>
-        <div>
-          <Redo2 />
-        </div>
+      <ToolButton
+          label="Undo"
+          icon={Undo2}
+          onClick={() => {}}
+          isDisabled={true}
+        />
+        <ToolButton
+          label="Redo"
+          icon={Redo2}
+          onClick={() => {}}
+          isDisabled={true}
+        />
       </div>
     </div>
   );
